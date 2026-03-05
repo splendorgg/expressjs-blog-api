e693aacdbee97305b55ad05a45748e7d83ab68ee
splendorgg <splendorg@protonmail.com>
2026-03-05 14:08:38 +0300

    feat: Add login method to AuthController

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,9 +1,12 @@
-import { register } from "#/modules/auth/auth.service.js"
+import { login, register } from "#/modules/auth/auth.service.js"
 
 export const AuthController = {
     register: async (req, res) => {
         const user = await register(req.body)
         res.status(201).json(user);
 
+    }
+    login: async (req, res) => {
+        
     }
 }
